c6eda3b2f2d501414cebb0f269b41c544ba112a6
Riyutokisaki <90891791+Riyutokisaki@users.noreply.github.com>
2022-06-28 22:31:04 +0800

    睡前存檔
    NPC腳本編寫中...

--- Stays/Assets/Script/Scenes5/Kid.cs
+++ Stays/Assets/Script/Scenes5/Kid.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Kid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region 欄位
+    [Header("對話資料")]
+    public DataDalogue dataDalogues;
+    [Header("對話系統")]
+    public DialongueSystem dialongueSystem;
+    [Header("觸發對象")]
+    public string target = "player";
+    #endregion
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+       // if (other.name == target) dialongueSystem.StartDialogue(dataDalogues.dialogues);
     }
 }

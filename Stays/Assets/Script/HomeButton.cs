469b9d6fb1a983f7412979cffaa0526011007fa3
Riyutokisaki <90891791+Riyutokisaki@users.noreply.github.com>
2022-08-04 22:53:09 +0800

    UI資訊業面建立
    把首頁UI能按到的畫面場景新增了!所以有增加UI需要的圖 放置在代用素材了!
    新增能做的:
    製作人員介紹文案
    字體選擇

--- Stays/Assets/Script/HomeButton.cs
+++ Stays/Assets/Script/HomeButton.cs
@@ -8,20 +8,28 @@ using UnityEngine.SceneManagement;
 public class HomeButton : MonoBehaviour
 {
     public string play;
+    public string setting;
+    public string movie;
+    public string about;
+   
     public void StartGame()
     {
         SceneManager.LoadScene(play);
     }
     public void Movie()
     {
-        SceneManager.LoadScene("");
+        SceneManager.LoadScene(movie);
     }
     public void Setting()
     {
-        SceneManager.LoadScene("");
+        SceneManager.LoadScene(setting);
     }
     public void Work()
     {
-        SceneManager.LoadScene("");
+        SceneManager.LoadScene(about);
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene("­º­¶");
     }
 }
